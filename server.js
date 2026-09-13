900a96d1d52c3a1b9f0a5b328bbdc494b53a1699
tauseef <ansaritauseef941238@gmail.com>
2026-09-13 07:31:09 +0000

    feat: ECS and fargate flow added

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`Hello from my docker v2 ${APP_NAME} ci/cd from docker github actions ECR and then EC2`);
+  res.send(`Hello from my docker v2 ${APP_NAME} ci/cd from docker github actions ECR and then (ECS + Fargate)`);
 });
 
 app.get("/health", (req, res) => {
   res.json({
     status: "OK",
-    message: "Server is running from docker and github action ECR and then EC2",
+    message: "Server is running from docker and github action ECR and then (ECS + Fargate)",
   });
 });
 
